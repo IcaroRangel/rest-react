b8030dbd81f1d9681a50389237e8f4529f0429f0
Icaro Cesar <icarorangel05@gmail.com>
2021-09-23 21:00:19 -0300

    add function addClothe and styled

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -11,27 +11,70 @@ interface ClotheProps {
 
 function App() {
   const [clothes, setClothes] = React.useState<ClotheProps[]>([]);
+  const [name, setName] = React.useState('');
+  const [description, setDescription] = React.useState('');
+  const [price, setPrice] = React.useState(0);
 
   const loadClothes = React.useCallback(async () => {
     const clothesResponse = await axios.get('http://localhost:3000/clothes');
     setClothes(clothesResponse.data);
   }, []);
+
   React.useEffect(() => {
     loadClothes();
   }, [loadClothes]);
+
+  const removeClothe = React.useCallback(
+    async (id: number) => {
+      await axios.delete(`http://localhost:3000/clothes/${id}`);
+      const deleteClothe = clothes.filter((clothe) => clothe.id !== id);
+      setClothes([...deleteClothe]);
+    },
+    [clothes],
+  );
+  const addClothe = React.useCallback(
+    async (e: any) => {
+      const response = { name, description, price };
+      await axios.post('http://localhost:3000/clothes', response);
+    },
+    [name, description, price],
+  );
+
   return (
-    <>
+    <Container>
       <h1>Roupas</h1>
-      <div>
-        {clothes.map((clothe) => (
-          <Container key={clothe.id}>
-            <h4>{clothe.name}</h4>
-            <p>{clothe.description}</p>
-            <p>Preço: R${clothe.price}</p>
-          </Container>
-        ))}
-      </div>
-    </>
+      <form onSubmit={addClothe}>
+        <label>Nome da roupa:</label>
+        <input
+          type="text"
+          value={name}
+          onChange={(e: any) => setName(e.target.value)}
+        />
+        <label>Descrição da roupa:</label>
+        <input
+          type="text"
+          value={description}
+          onChange={(e: any) => setDescription(e.target.value)}
+        />
+        <label>Preço da roupa:</label>
+        <input
+          type="text"
+          value={price}
+          onChange={(e: any) => setPrice(e.target.value)}
+        />
+        <button style={{ marginLeft: '14rem', marginTop: '-2.6rem' }}>
+          Enviar
+        </button>
+      </form>
+      {clothes.map((clothe) => (
+        <div key={clothe.id}>
+          <h4>{clothe.name}</h4>
+          <p>{clothe.description}</p>
+          <p>Preço: R${clothe.price}</p>
+          <button onClick={() => removeClothe(clothe.id)}>Remover Roupa</button>
+        </div>
+      ))}
+    </Container>
   );
 }
 
--- frontend/src/app/styles.ts
+++ frontend/src/app/styles.ts
@@ -1,8 +1,28 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  height: 20vh;
+  height: 30vh;
+  font-size: 18px;
   h4 {
     text-transform: capitalize;
+  }
+  button {
+    position: absolute;
+    background: #fff;
+    border-radius: 30px;
+    cursor: pointer;
+    margin-bottom: 5px;
+  }
+  input {
+    display: block;
+    margin-bottom: 20px;
+  }
+  div,
+  form {
+    padding-left: 6px;
+    border: 1px solid #cc0000;
+    border-radius: 6px;
+    height: 30vh;
+    margin-bottom: 20px;
   }
 `;
